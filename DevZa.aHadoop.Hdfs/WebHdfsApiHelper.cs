5f359edec8323147d8bf1e7b7c72c35293ee8f6f
Develop Zakk <DevelopZakk@outlook.com>
2016-05-25 14:22:14 +0800

    rename

--- DevZa.aHadoop.Hdfs/WebHdfsApiHelper.cs
+++ DevZa.aHadoop.Hdfs/WebHdfsApiHelper.cs
@@ -11,52 +11,39 @@ namespace DevZa.aHadoop.Hdfs
             get { return AHadoopConfigurationManager.AHadoopConfig; }
         }
 
-        private static AHadoopUserInfoConfig UserInfoConfig
-        {
-            get { return AHadoopConfigurationManager.UserInfoConfig; }
-        }
+        private static AHadoopUserInfoConfig UserInfoConfig => AHadoopConfigurationManager.UserInfoConfig;
 
-        public static string GetBaseUrlString()
-        {
-            return string.Format("http://{0}:{1}", AHadoopConfig.ActiveHadoopNode.Ip, AHadoopConfig.WebHdfs.NameNodePort);
-        }
+        public static string GetBaseUrlString() => $"http://{AHadoopConfig.ActiveHadoopNode.Ip}:{AHadoopConfig.WebHdfs.NameNodePort}";
 
-        public static string GetDataNodeBaseUrlString()
-        {
-            return string.Format("http://{0}:{1}", AHadoopConfig.FirstDataNode.Ip, AHadoopConfig.WebHdfs.DataNodePort);
-        }
+        public static string GetDataNodeBaseUrlString() => $"http://{AHadoopConfig.FirstDataNode.Ip}:{AHadoopConfig.WebHdfs.DataNodePort}";
 
-        public static string GetNameNodeFullUrlWithApiPrefix(string subpathuri)
-        {
-            return string.Format("{0}{1}", AHadoopConfig.WebHdfs.ApiPrefix, subpathuri);
-        }
+        public static string GetNameNodeFullUrlWithApiPrefix(string subpathuri) => $"{AHadoopConfig.WebHdfs.ApiPrefix}{subpathuri}";
 
         public static string GetOperationAndUserName(string operation)
         {
             return GetOperationAndUserName(operation, UserInfoConfig.User.Id);
         }
-        private static string GetOperationAndUserName(string operation, string username)
-        {
-            return string.Format("op={0}&user.name={1}", operation, username);
-        }
+        private static string GetOperationAndUserName(string operation, string username) => $"op={operation}&user.name={username}";
 
         public static string GetListsStatusUri(string path)
         {
             LinuxPathUtility.CheckPathStartWithSlashorNot(path);
-            return GetNameNodeFullUrlWithApiPrefix(string.Format("{0}?{1}", path, GetOperationAndUserName(WebHdfsAPIConstants.Status.ListsStatus)));
+            return GetNameNodeFullUrlWithApiPrefix(
+                $"{path}?{GetOperationAndUserName(WebHdfsAPIConstants.Status.ListsStatus)}");
         }
 
         public static string GetGetFileStatusUri(string path)
         {
             LinuxPathUtility.CheckPathStartWithSlashorNot(path);
-            return GetNameNodeFullUrlWithApiPrefix(string.Format("{0}?{1}", path, GetOperationAndUserName(WebHdfsAPIConstants.Status.GetFileStatus)));
+            return GetNameNodeFullUrlWithApiPrefix(
+                $"{path}?{GetOperationAndUserName(WebHdfsAPIConstants.Status.GetFileStatus)}");
         }
 
         public static string CreateSubFolderUri(string subpath)
         {
             return
-                GetNameNodeFullUrlWithApiPrefix(string.Format("{0}?{1}", subpath,
-                    GetOperationAndUserName(WebHdfsAPIConstants.Status.CreateFolder)));
+                GetNameNodeFullUrlWithApiPrefix(
+                    $"{subpath}?{GetOperationAndUserName(WebHdfsAPIConstants.Status.CreateFolder)}");
         }
 
         public static string DeleteSubFolderOrFileUri(string subpath, bool recursive =false)
@@ -66,18 +53,10 @@ namespace DevZa.aHadoop.Hdfs
                     GetOperationAndUserName(WebHdfsAPIConstants.Status.RemoveFolderOrFile),recursive));
         }
 
-        public static string GetCreateFileUri(string path, bool overwrite)
-        {
-            return
-                GetNameNodeFullUrlWithApiPrefix(string.Format("{0}?{1}&overwrite={2}", path,
-                    GetOperationAndUserName(WebHdfsAPIConstants.Status.CreateAndWriteToAFile),overwrite));
-        }
+        public static string GetCreateFileUri(string path, bool overwrite) => GetNameNodeFullUrlWithApiPrefix(
+            $"{path}?{GetOperationAndUserName(WebHdfsAPIConstants.Status.CreateAndWriteToAFile)}&overwrite={overwrite}");
 
-        public static string OpenFileUri(string fileWithPath)
-        {
-            return
-                GetNameNodeFullUrlWithApiPrefix(string.Format("{0}?{1}", fileWithPath,
-                    GetOperationAndUserName(WebHdfsAPIConstants.Status.Open)));
-        }
+        public static string OpenFileUri(string fileWithPath) => GetNameNodeFullUrlWithApiPrefix(
+            $"{fileWithPath}?{GetOperationAndUserName(WebHdfsAPIConstants.Status.Open)}");
     }
 }
